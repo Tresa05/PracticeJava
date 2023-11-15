0cc5aeeacef4f8aeb0d03a92eff46987993682ca
Tresa05 <tresamariya05@gmail.com>
2023-11-14 21:54:11 -0500

    Assignment to analyse height for amusement park entrance

---
+++ src/amusementpark/HeightRequirements.java
@@ -0,0 +1,31 @@
+package amusementpark;
+
+public class HeightRequirements {
+	int height;
+public static void main(String[] args) {
+		
+		HeightRequirements Access=new HeightRequirements();
+		Access.height=120;
+		
+		if(Access.height>=90&&Access.height<=200) {
+			
+			System.out.println("You are eligible to enter the park");
+			
+		}
+		else {
+			
+			System.out.println("You are not eligible to enter the park");
+			
+		}
+	}
+
+}
+
+	
+	
+	
+	
+
+		
+	
+		
