3b2d52895d174f127b3212cde27137f72e817b3c
Tresa05 <tresamariya05@gmail.com>
2023-11-14 22:21:56 -0500

    Assigment amusementpark modified to add comments

--- src/amusementpark/HeightRequirements.java
+++ src/amusementpark/HeightRequirements.java
@@ -3,9 +3,12 @@ package amusementpark;
 public class HeightRequirements {
 	int height;
 public static void main(String[] args) {
-		
+	    // TODO Auto-generated method stub
+	
 		HeightRequirements Access=new HeightRequirements();
 		Access.height=120;
+		//here I am considering people less than the height of 16 Height<90 centimeter and more than 200 centimeter is not allowed.
+		//and whoever falls within this height requirement is eligible to enter the park.
 		
 		if(Access.height>=90&&Access.height<=200) {
 			
